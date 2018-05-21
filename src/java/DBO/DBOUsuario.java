1d82bfc540cc40b8e59302e27d592f9c10967be9
Gabriel Fronio <gabrielfronio@gmail.com>
2018-05-20 21:59:20 -0300

    DBOs e DAOs iniciados

---
+++ src/java/DBO/DBOUsuario.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBO;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author gabri
+ */
+public class DBOUsuario {
+
+    public DBOUsuario(String nome, String cpf, String tel, String cep, String end, String email, String senha, char sexo, Date data) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.tel = tel;
+        this.cep = cep;
+        this.end = end;
+        this.email = email;
+        this.senha = senha;
+        this.sexo = sexo;
+        this.data = data;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getId() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setId(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the tel
+     */
+    public String getTel() {
+        return tel;
+    }
+
+    /**
+     * @param tel the tel to set
+     */
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the end
+     */
+    public String getEnd() {
+        return end;
+    }
+
+    /**
+     * @param end the end to set
+     */
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public char getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the data
+     */
+    public Date getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
+    private String nome, cpf, tel, cep, end, email, senha;
+    private char sexo;
+    private Date data;
+    
+    
+}
